27c599f7e0e36230f7888347bddc3665ad6b76e5
souravangra <angrasourav@yahoo.com>
2017-06-20 23:20:32 +0530

    First Commit

---
+++ App_BAL/DataTransfer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_BAL
+{
+    public class DataTransfer
+    {
+
+    }
+
+    public enum EmOrderType
+    {
+        TakeAway = 1,
+        Delivery = 2
+    }
+    public enum EmPaymentType
+    {
+        Cash = 1,
+        Card = 2
+    }
+    public class CartCL
+    {
+        public int SrNo { get; set; }
+        public int OrderID { get; set; }
+        public string OrderNo { get; set; }
+        public EmOrderType OrderType { get; set; }
+        public List<CartItemsCL> Items { get; set; }
+        public bool IsOrderConfirmed { get; set; }
+        public EmOrderType PaymentType { get; set; }
+    }
+
+    public class CartItemsCL
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int CategoryID { get; set; }
+        public double Price { get; set; }
+    }
+
+    public class CategoryListCL
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+    }
+
+    public class ProductListCL
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+        public int CategoryID { get; set; }
+        public string ImageName { get; set; }
+    }
+}
---
+++ App_UI/UserControls/uc_CategoryMenu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App_UI.UserControls
+{
+    public partial class uc_CategoryMenu : UserControl
+    {
+        public uc_CategoryMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
